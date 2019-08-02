8e122efe0ba7fba2af4340ad19544bc975c64aea
Alan <HanYLun@nit.net.cn>
2019-08-02 21:24:55 +0800

    2019.8.2 night

--- Practice/src/Que15.java
+++ Practice/src/Que15.java
@@ -12,7 +12,7 @@ public class Que15 {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-             
+              
         List<List<Integer>> resultList = new ArrayList<List<Integer>>();
 
         for (int i = 0; i < nums.length - 2; i++) {
