feca7a9ec99a19e6797ab21928af13f908af8682
Han-YLun <ArvinHan@foxmail.com>
2021-03-28 14:47:23 +0800

    Added 添加题解及题目链接

--- src/Que268.java
+++ src/Que268.java
@@ -16,6 +16,7 @@ public class Que268 {
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 0){
                 return i;
+                
             }
         }
         return 0;
