40c3a565f4511f7f780141ac3dfc811ee44b6f91
Han-YLun <ArvinHan@foxmail.com>
2021-04-10 12:43:53 +0800

    Updated 更新28题解

--- src/Que28.java
+++ src/Que28.java
@@ -7,34 +7,34 @@ public class Que28 {
 
     public int strStr(String haystack, String needle) {
 
-        if (needle.equals("")){
+        if (needle == null || "".equals(needle)) {
             return 0;
         }
 
         int index = 0;
 
         for (int i = 0; i < haystack.length(); i++) {
-            if (haystack.charAt(i) == needle.charAt(index)){
-                i = i+1;
-                int j =index+1;
-               while(j < needle.length()){
-                    if (i >= haystack.length()){
+            if (haystack.charAt(i) == needle.charAt(index)) {
+                i = i + 1;
+                int j = index + 1;
+                while (j < needle.length()) {
+                    if (i >= haystack.length()) {
                         return -1;
                     }
 
-                    if (haystack.charAt(i) == needle.charAt(j)){
+                    if (haystack.charAt(i) == needle.charAt(j)) {
                         i++;
                         j++;
-                    }else{
+                    } else {
                         break;
                     }
                 }
 
-               if (j >= needle.length()){
-                   return i-needle.length();
-               }
+                if (j >= needle.length()) {
+                    return i - needle.length();
+                }
 
-               i -= j;
+                i -= j;
             }
         }
 
@@ -42,34 +42,34 @@ public class Que28 {
     }
 
     public int strStr1(String haystack, String needle) {
-        if (needle.equals("")){
+        if (needle.equals("")) {
             return 0;
         }
 
         char[] hayArr = haystack.toCharArray();
         char[] needArr = needle.toCharArray();
 
-        int i=0, j = 0;
+        int i = 0, j = 0;
 
-        while (i < hayArr.length && j < needArr.length){
+        while (i < hayArr.length && j < needArr.length) {
 
-            if (hayArr[i] == needArr[j]){
+            if (hayArr[i] == needArr[j]) {
                 i++;
                 j++;
-            }else{
-                i = i -j +1;
-                j =0;
+            } else {
+                i = i - j + 1;
+                j = 0;
             }
         }
 
-        if (j >= needArr.length){
+        if (j >= needArr.length) {
             return i - j;
-        }else{
+        } else {
             return -1;
         }
 
     }
-    
+
 
     public static void main(String[] args) {
         Que28 que28 = new Que28();
