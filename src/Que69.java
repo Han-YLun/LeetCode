d6d527723ca49c2557597230cc00c51c2b033cf6
Han-YLun <ArvinHan@foxmail.com>
2021-04-14 21:03:44 +0800

    Updated 更新69题解

--- src/Que69.java
+++ src/Que69.java
@@ -7,20 +7,20 @@ public class Que69 {
     //二分法求解
     public int mySqrt(int x) {
 
-        if (x == 0 || x == 1){
+        if (x == 0 || x == 1) {
             return x;
         }
 
-        int left = 1, right = x , result = 0;
-        while (left <= right){
+        int left = 1, right = x, result = 0;
+        while (left <= right) {
             int middle = (left + right) / 2;
 
 
-            if (middle == (x / middle)){
+            if (middle == (x / middle)) {
                 return middle;
-            }else if (middle > (x / middle)){
+            } else if (middle > (x / middle)) {
                 right = middle - 1;
-            }else if (middle < (x / middle)){
+            } else if (middle < (x / middle)) {
                 left = middle + 1;
                 result = middle;
             }
@@ -29,6 +29,33 @@ public class Que69 {
         return result;
 
     }
-    
-    
+
+    public int mySqrt1(int x) {
+
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int left = 1, right = x, result = 0;
+        while (left <= right) {
+            int middle = left + (right-left) / 2;
+
+
+            if ((long) middle * middle <= x) {
+                result = middle;
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Que69().mySqrt1(2147483647));
+    }
+
+
 }
