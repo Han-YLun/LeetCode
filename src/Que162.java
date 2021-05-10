72aea0b284fc42358feda10a3828b957d0a061e7
Han-YLun <ArvinHan@foxmail.com>
2021-05-10 22:31:45 +0800

    Updated 更新162题解

--- src/Que162.java
+++ src/Que162.java
@@ -1,8 +1,18 @@
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.awt.event.KeyAdapter;
+
 /**
  * @author: hyl
  * @date: 2021/05/10
  **/
 public class Que162 {
+
+    /**
+     * 时间复杂度: O(N)
+     * @param nums
+     * @return
+     */
     public int findPeakElement(int[] nums) {
 
         //处理只有一个的情况
@@ -32,4 +42,45 @@ public class Que162 {
         }
         return 0;
     }
+
+    /**
+     * 时间复杂度: O(N)
+     * @param nums
+     * @return
+     */
+    public int findPeakElement1(int[] nums) {
+
+        //使用连续的两个元素 nums[j]nums[j] 和 nums[j + 1]nums[j+1] 不会相等
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] > nums[i+1]){
+                return i;
+            }
+        }
+        return nums.length-1;
+    }
+
+    /**
+     * 时间复杂度 : O(logN)
+     * 空间复杂度 : O(logN)
+     * @param nums
+     * @return
+     */
+    public int findPeakElement2(int[] nums) {
+        return search(nums,0,nums.length-1);
+    }
+
+    private int search(int[] nums, int left, int right) {
+        if (left == right){
+            return left;
+        }
+
+        int mid = (left + right) / 2;
+        if (nums[mid] > nums[mid+1]){
+            return search(nums,left,mid);
+        }else{
+            return search(nums,mid+1,right);
+        }
+
+    }
+
 }
