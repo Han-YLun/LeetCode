dc727e976c21112e1c9a9e7b671d636788a4a5e1
Han-YLun <ArvinHan@foxmail.com>
2021-05-12 20:17:44 +0800

    Updated 更新34题解

--- src/Que34.java
+++ src/Que34.java
@@ -37,11 +37,43 @@ public class Que34 {
         return pos;
     }
 
- 
+    /**
+     * 时间复杂度 : O(logN)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange1(int[] nums, int target) {
+
+        int leftIndex = binarySearch(nums,target,true);
+        int rightIndex = binarySearch(nums,target,false);
+
+        if (leftIndex <= rightIndex && rightIndex< nums.length
+        && nums[leftIndex] == target && nums[rightIndex] == target){
+            return new int[]{leftIndex,rightIndex};
+        }
+        return new int[]{-1,-1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length-1,ans = nums.length;
+        while (left < right){
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)){
+                right = mid - 1;
+                ans = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Que34().searchRange(
-                new int[]{1, 3}
-                , 1)));
+        System.out.println(Arrays.toString(new Que34().searchRange1(
+                new int[]{5,7,7,8,8,10}
+                , 8)));
     }
 }
