f64bc10239db3e1e96b6595bb9bb933dfded8e9c
Han-YLun <ArvinHan@foxmail.com>
2021-05-15 12:24:09 +0800

    Updated 更新238题解

--- src/Que238.java
+++ src/Que238.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 /**
  * @author: hyl
@@ -56,6 +57,11 @@ public class Que238 {
         }
 
         return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                Arrays.toString(new Que238().productExceptSelf(new int[]{1, 2})));
     }
 
 }
