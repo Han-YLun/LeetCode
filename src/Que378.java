7ed0a94d31b2f7aca09e09f76b2e93fa73008188
Han-YLun <ArvinHan@foxmail.com>
2021-05-23 16:15:27 +0800

    Updated 更新378题解

--- src/Que378.java
+++ src/Que378.java
@@ -1,3 +1,5 @@
+import sun.nio.cs.ext.MacArabic;
+
 import java.util.Arrays;
 
 /**
@@ -10,9 +12,10 @@ public class Que378 {
      * 直接将矩阵放到数组中,然后排序后查询第K小元素即可
      * 时间复杂度 ： O(N)
      * 空间复杂度 : O(N)
-     * @param matrix    矩阵
-     * @param k 排序后的第 k 小元素
-     * @return  排序后的第 k 小元素
+     *
+     * @param matrix 矩阵
+     * @param k      排序后的第 k 小元素
+     * @return 排序后的第 k 小元素
      */
     public int kthSmallest(int[][] matrix, int k) {
 
@@ -20,13 +23,64 @@ public class Que378 {
         int len = matrix[0].length;
         int[] arr = new int[size * len];
         int index = 0;
-        for (int i = 0; i < size; i++) {
+        for (int[] matrix1 : matrix) {
             for (int j = 0; j < len; j++) {
-                arr[index++] = matrix[i][j];
+                arr[index++] = matrix1[j];
             }
         }
 
         Arrays.sort(arr);
-        return arr[k-1];
+        return arr[k - 1];
     }
+
+    /**
+     * 直接将矩阵放到数组中,然后排序后查询第K小元素即可
+     * 时间复杂度 ： O(N * N * logN)
+     * 空间复杂度 : O(N * N)
+     *
+     * @param matrix 矩阵
+     * @param k      排序后的第 k 小元素
+     * @return 排序后的第 k 小元素
+     */
+    public int kthSmallest1(int[][] matrix, int k) {
+        int size = matrix.length;
+        int len = matrix[0].length;
+
+        //最小值
+        int left = matrix[0][0];
+        //最大值
+        int right = matrix[size - 1][len - 1];
+
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, size)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private boolean check(int[][] matrix, int mid, int k, int size) {
+
+        int i = size - 1;
+        int j = 0;
+        int num = 0;
+        while ( (i >= 0) && (j <size)){
+            if (matrix[i][j] <= mid){
+                num += (i+1);
+                j++;
+            }else{
+                i--;
+            }
+        }
+        return num >= k;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix  = new int[][]{{1,5,9},{10,11,13},{12,13,15}};
+        System.out.println(new Que378().kthSmallest1(matrix,8));
+    }
+
 }
